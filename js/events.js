007e9f9d5a3091efe574d0453ab1cb0b60cb0991
sabbir1257 <masabbir1257@gmail.com>
2023-02-11 00:39:21 +0600

    push

--- js/events.js
+++ js/events.js
@@ -1,7 +1,11 @@
 //  option 1: directly set on the HTML element
+//  <button onclick="console.log(65)">Another Button</button>  
 
 
-// option 2: add onclik function
+// option 2: add onclik function on the html element
+// IMPROTANT: We will use this
+//    <button onclick="makeRed()">Make Red</button>
+
 function makeRed(){
     document.body.style.backgroundColor = 'red' ;
 }
@@ -21,3 +25,27 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple(){
     document.body.style.backgroundColor = 'purple';
 }
+
+
+// option 4: another
+const pinkBullon = document.getElementById('make-pink');
+     pinkBullon.addEventListener('click', makePink);
+
+
+     function makePink(){
+        document.body.style.backgroundColor = 'pink';
+     }
+
+
+ // option 4: another
+ const greenButton = document.getElementById('make-green');
+ greenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+ })
+
+
+//  option 4 : final
+// IMPROTANT: We will use this sometimes
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+} )
