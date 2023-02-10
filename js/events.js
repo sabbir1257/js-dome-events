fe39a83036c4f8307c7fd6799d9b6d6f2cd0950d
sabbir1257 <masabbir1257@gmail.com>
2023-02-10 18:59:16 +0600

    push

--- js/events.js
+++ js/events.js
@@ -1,4 +1,5 @@
-console.log('This is separate Js file');
+//  option 1: directly set on the HTML element
+
 
 // option 2: add onclik function
 function makeRed(){
@@ -6,10 +7,17 @@ function makeRed(){
 }
 
 
-//  option 2:
+//  option 3:
 const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick = makeBlue;
 
 function makeBlue(){
     document.body.style.backgroundColor = 'blue' ;
+}
+
+
+//  option 3: another
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple';
 }
